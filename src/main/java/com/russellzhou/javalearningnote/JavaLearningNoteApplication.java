e163c0de805e39a2c82bb0c50b584e28d0448195
barbatos24 <zhoudewei_86@163.com>
2021-11-07 11:25:05 +0800

    init commit

---
+++ src/main/java/com/russellzhou/javalearningnote/JavaLearningNoteApplication.java
@@ -0,0 +1,13 @@
+package com.russellzhou.javalearningnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLearningNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaLearningNoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/russellzhou/javalearningnote/JavaLearningNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.russellzhou.javalearningnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLearningNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
